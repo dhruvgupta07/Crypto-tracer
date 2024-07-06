07124407cefbad5457024d16968d745fd1d1a819
Dhruv Gupta <dhruvg-mca23@cs.du.ac.in>
2024-07-06 14:14:40 +0530

    charts completed

--- javascript/details.js
+++ javascript/details.js
@@ -10,6 +10,9 @@ function loadDetail() {
     fetch(`https://api.coingecko.com/api/v3/coins/${params.get('id')}?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false`)
         .then(convertToJSON)
         .then(render);
+    fetch(`https://api.coingecko.com/api/v3/coins/${params.get('id')}/market_chart?vs_currency=inr&days=3&interval=daily`)
+        .then(convertToJSON)
+        .then(renderChart);
 }
 
 function convertToJSON(response) {
@@ -38,4 +41,48 @@ function render(data) {
 
 window.onload = function() {
     loadDetail();
+}
+
+function renderChart(data) {
+    const prices = data.prices;
+    const timesstamps = [];
+    const prices_inr = [];
+    
+    // Extract timestamps and prices_inr from prices array
+    for (let i = 0; i < prices.length; i++) {
+        const single_price = prices[i];
+        const date_obj=new Date(single_price[0]);
+        let hours=date_obj.getHours();
+        if(hours < 10){
+            hours = "0" + hours;
+        }
+        let minutes=date_obj.getMinutes();
+        if(minutes<10){
+            minutes="0"+minutes;
+        }
+        timesstamps.push(`${hours}:${minutes}`);
+        prices_inr.push(single_price[1]);
+    }
+
+    const ctx = document.getElementById('myChart');
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: timesstamps,  // Use timestamps array for labels
+            datasets: [{
+                label: 'Price in INR',  // Update label to reflect the data
+                data: prices_inr,       // Use prices_inr array for the data
+                borderColor: 'rgba(75, 192, 192, 1)', // Example color (adjust as needed)
+                backgroundColor: 'rgba(75, 192, 192, 0.2)', // Example background color
+                borderWidth: 1
+            }]
+        },
+        options: {
+            plugins:{
+                legend:{
+                    display:false,
+                }
+            }
+        }
+    });
 }
