0ee3ad19260bf30b51fea9ce0419cd2e330379ad
Dhruv Gupta <dhruvg-mca23@cs.du.ac.in>
2024-07-06 09:38:09 +0530

    responsive+fixed frontend

--- javascript/javasc.js
+++ javascript/javasc.js
@@ -1,32 +1,3 @@
-
-var loginContainer = document.querySelector('.wrapper');
-var registerContainer = document.querySelector('.wrapper2');
-var registerLink = document.querySelector('.registerLink');
-var registerLink2 = document.querySelector('.registerLink2');
-registerLink.addEventListener('click', function(event) {
-  event.preventDefault(); // Prevent the default behavior of the link
-
-  // Toggle display of the containers
-  loginContainer.style.display = 'none';
-  registerContainer.style.display = 'block';
-})
-registerLink2.addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent the default behavior of the link
-  
-    // Toggle display of the containers
-    registerContainer.style.display = 'none';
-    loginContainer.style.display = 'block';
-    
-  });
-
-   
-
-
-
-
-
-
-
-
-
-
+function openprice() {
+  window.location.href = 'prices.html'; // Replace 'prices.html' with the actual URL of the prices page
+}
